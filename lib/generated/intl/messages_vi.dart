c9c1ce879d9dd726569a40b75cafa0e2d183801b
Minh Giang <minhgiang241@gmail.com>
2023-10-04 11:24:20 +0700

    change text

--- lib/generated/intl/messages_vi.dart
+++ lib/generated/intl/messages_vi.dart
@@ -476,7 +476,7 @@ class MessageLookup extends MessageLookupByLibrary {
         "enter_worker_num":
             MessageLookupByLibrary.simpleMessage("Nhập số công nhân"),
         "env_fee":
-            MessageLookupByLibrary.simpleMessage("Ngày kết thúc thi công"),
+            MessageLookupByLibrary.simpleMessage("Phí bảo vệ môi truòng"),
         "err_conn": MessageLookupByLibrary.simpleMessage("Lỗi kết nối "),
         "err_internet": MessageLookupByLibrary.simpleMessage(
             "Không kết nối được với máy chủ"),
